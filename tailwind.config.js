ff514c1bdda2989df50890c449b3dbc3853dcad9
Hp <hafsanoor401@gmail.com>
2024-12-05 16:18:08 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html", // Include all HTML files in the `public` folder
+    "./src/**/*.{html,js}" // Include any files in the `src` folder (if applicable)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
